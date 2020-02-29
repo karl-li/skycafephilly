871c1c48355e7fd3898452b39915faa1d8b05c48
Karl Li <karl-li@users.noreply.github.com>
2020-02-29 17:08:59 -0500

    added Google Map view of Sky Cafe

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
+//initialize Accordion at 1000px viewport
 getAccordion("#tabs",1000);
+
+//run openNav function
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
-  
-  function closeNav() {
+
+//run closeNav function
+function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+}
+
+// Initialize and add the Google map
+function initMap() {
+  // The location of Sky Cafe
+  var skyCafe = {lat: 39.937037, lng: -75.162849};
+  // The map, centered at Sky Cafe
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 18, center: skyCafe});
+  // The marker, positioned at Sky Cafe
+  var marker = new google.maps.Marker({position: skyCafe, map: map});
 }
